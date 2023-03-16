1d3f0faee35cb6824a1f803b7660723e736d340d
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-16 10:34:06 -0600

    Repository Init

---
+++ src/Distribuidos_Paralelos/RequisitoExamen.java
@@ -0,0 +1,7 @@
+package Distribuidos_Paralelos;
+
+public class RequisitoExamen {
+	public static void main(String[] args) {
+		System.out.println("Sexo");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Antonio Noguera
+ *
+ */
+module Distribuidos_Paralelos {
+}
